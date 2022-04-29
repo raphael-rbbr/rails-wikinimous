03540af1464afd7d60d903dbf42a01605f7bddd5
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-04-29 20:01:00 -0300

    ..

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -24,13 +25,30 @@ class ArticlesController < ApplicationController
   def edit
   end
 
+
+  # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /articles/1
   def destroy
+    @article.destroy
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
 
-  def restaurant_params
+  def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
